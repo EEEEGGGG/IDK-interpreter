3efb6667da2f4a910d354d7034f0bab58bc9311f
GrpeApple <grapeapplemelonwater@gmail.com>
2021-01-30 06:32:36 +0800

    Edit a lot of code, but I think I rewrote it.

--- idk.sh
+++ idk.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash 
 
-# Set to enable Debugging
-#set -xv
-#DEBUG=1
-# Convert to tokens
-function token () {
+# Uncomment to enable debugging
+##set -xv
+##DEBUG=1
+
+# stdin
+function stdin () {
+	## Get input and put it into an array to TOKENS
 	readarray -t TOKENS
 }
 
+# file source
+function src () {
+	## Same as stdin but changed the name because it makes more sense
+	## Get file and change it to an array to TOKENS
+	mapfile -t TOKENS < "$1"
+}
+
+# Run the interpreter
 function interpreter () {
+	## Check if BOF exists
 	if [[ ${TOKENS[0]} != openC ]]; then
 		exit 1
 	fi
@@ -41,18 +52,70 @@ function interpreter () {
 		esac
 	done
 	
+	## Check if EOF exists
 	if [[ ${TOKENS[-1]} != closeC ]]; then
 		echo "Error, no 'closeC' at EOF"
 		exit 2
 	fi
 }
 
-if [[ -n $@ ]]; then
-	for file in "$@"; do
-		mapfile -t TOKENS < "$file"
-	done
+# Show usage
+##function usage () {
+##	echo "stdin: $0 -s"
+##}
+
+# Set to enable Debugging
+##while getopts ":hd:sf:" option; do
+##	case $option in
+##		h)
+##			usage
+##		;;
+##		d)
+##			case "$OPTARG" in
+##				"")
+##					DEBUG=1
+##				;;
+##				1)
+##					DEBUG=1
+##				;;
+##				2)
+##					set -xv
+##				;;
+##				3)
+##					DEBUG=1
+##					set -xv
+##				;;
+##				*)
+##					echo "Invalid DEBUG value: $OPTARG. Use 1, 2, 3"
+##					exit 3
+##				;;
+##			esac
+##		;;
+##		s)
+##			stdin
+##		;;
+##		f)
+##			if [[ -n $OPTARG ]]; then
+##				for file in $OPTARG; do
+##					src "$file"
+##				done
+##			else
+##				echo "Option -f defined but no argument."
+##				exit 4
+##			fi
+##		;;
+##		*)
+##			echo "Unknown option"
+##	esac
+##done
+
+# Check if no argument is provided
+if [[ $# -eq 0 ]]; then
+	stdin
 else
-	token
+	for file in $@; do
+		src $file
+	done
 fi
 # Check DEBUG
 if [[ -n $DEBUG ]]; then
