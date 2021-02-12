8a9eda55c7c3ca6b476b942c094643e1c72acd63
GrpeApple <grapeapplemelonwater@gmail.com>
2021-02-12 23:22:21 +0800

    more C++

--- C++/idk.cpp
+++ C++/idk.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <cstdio>
 using namespace std;
 
+int error_exit(char error_msg[]) {
+	printf("\aerror: %s", error_msg);
+	return 1;
+}
+
 int stdinMode() {
-	cout << "stdin";
+	cout << "stdin ";
+	return 0;
+}
+
+int fileMode(string file) {
+	cout << "file: " << file << " ";
 	return 0;
 }
 
 int debugMode() {
-	cout << "debug";
+	cout << "debug ";
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
 	for ( int i = 1; i < argc; ++i ) {
-		string argument = argv[i];
+		string argument = argv[i]; // bad, please fix.
 		if ( argument == "-d" ) {
 			debugMode();
 		} else if ( argument == "-s" ) {
 			stdinMode();
+		} else if ( argument == "-f" ) {
+			fileMode(argv[i + 1]); // bad, please fix.
+			i++;
 		}
 	}
 	return 0;
